81f37b3fe2eb80df92d9b03bb1a2e5b171993ccb
Nuria Gardenyes <gardenyesnuria@gmail.com>
2021-04-28 23:22:46 +0200

    formValidate.js modificat

--- formValidate.js
+++ formValidate.js
@@ -1,24 +1,28 @@
 function loginValidation(){
     
-
     let emailUsuari = document.getElementById("email");
     let passwordUsuari = document.getElementById("pwd");
-
+    let isright = true;
 
    if (passwordUsuari.value.trim() == ""){
        passwordUsuari.classList.add("is-invalid");
-       document.getElementById("errorPassword").innerHTML = "Camp obligatori"
+       document.getElementById("errorPassword").innerHTML = "Camp obligatori";
+       isright = false;
    } 
 
    let patro = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
    if (emailUsuari.value.trim() == ""){
            emailUsuari.classList.add("is-invalid");
-           document.getElementById("errorEmail").innerHTML = "Camp obligatori"
+           document.getElementById("errorEmail").innerHTML = "Camp obligatori";
+           isright = false;
 
     }else if(patro.test(emailUsuari.value)==false){
         emailUsuari.classList.add("is-invalid");
-        document.getElementById("errorEmail").innerHTML = "format mail incorrecte"
+        document.getElementById("errorEmail").innerHTML = "format mail incorrecte";
+        isright = false;
     }
+
+    return isright;
 
 }
 
@@ -27,18 +31,24 @@ function loginValidation(){
 function searchValidar(){
     
     let searchUsuari;
+    let is_right = true;
+
 
     searchUsuari = document.getElementById("form1");
     let searchUsuariV = searchUsuari.value.trim();
 
     if (searchUsuari.value.trim() == ""){
         searchUsuari.classList.add("is-invalid");
-        document.getElementById("errorSearch").innerHTML = "Camp obligatori"
+        document.getElementById("errorSearch").innerHTML = "Camp obligatori";
+        is_right = false;
 
     }else if(searchUsuariV.length<3){
         searchUsuari.classList.add("is-invalid");
         document.getElementById("errorSearch").innerHTML = "minim 3 caracters"
+        is_right = false;
     }
+
+    return is_right;
 }
 
 
@@ -52,6 +62,7 @@ function registreValidation(){
     let passwordConfirm;
     let provinciaReg;
     let patroEmail;
+    let isRight = true;
 
     nameUsuariReg = document.getElementById("fname");
     cognomUsuariReg = document.getElementById("lname");
@@ -67,9 +78,12 @@ function registreValidation(){
     if (nameUsuariReg.value.trim() == ""){
         nameUsuariReg.classList.add("is-invalid");
         document.getElementById("errorName").innerHTML = "Camp obligatori";
+        isRight=false;
+
     }else if(patroNomUsuari.test(nameUsuariReg.value)==false){
         nameUsuariReg.classList.add("is-invalid");
         document.getElementById("errorName").innerHTML = "Format nom incorrecte";
+        isRight=false;
     }
 
 
@@ -77,10 +91,12 @@ function registreValidation(){
     if (cognomUsuariReg.value.trim() == ""){
         cognomUsuariReg.classList.add("is-invalid");
         document.getElementById("errorCognom").innerHTML = "Camp obligatori";
+        isRight=false;
 
     }else if(patroNomUsuari.test(cognomUsuariReg.value)==false){
         cognomUsuariReg.classList.add("is-invalid");
         document.getElementById("errorCognom").innerHTML = "Format cognom incorrecte";
+        isRight=false;
     }
 
 
@@ -89,10 +105,12 @@ function registreValidation(){
     if (emailUsuariReg.value.trim() == ""){
         emailUsuariReg.classList.add("is-invalid");
         document.getElementById("errorEmailReg").innerHTML = "Camp obligatori";
+        isRight=false;
 
     }else if(patroEmail.test(emailUsuariReg.value)==false){
         emailUsuariReg.classList.add("is-invalid");
-        document.getElementById("errorEmailReg").innerHTML = "format mail incorrecte"
+        document.getElementById("errorEmailReg").innerHTML = "format mail incorrecte";
+        isRight=false;
     }
 
 
@@ -100,21 +118,27 @@ function registreValidation(){
     if (passwordUsuariReg.value.trim() == ""){
         passwordUsuariReg.classList.add("is-invalid");
         document.getElementById("errorPasswordReg").innerHTML = "Camp obligatori";
+        isRight=false;
     }
 
 
     if (passwordConfirm.value.trim() == ""){
         passwordConfirm.classList.add("is-invalid");
         document.getElementById("errorPasswordConf").innerHTML = "Camp obligatori";
+        isRight=false;
 
     }else if (passwordUsuariReg.value !== passwordConfirm.value){
         passwordConfirm.classList.add("is-invalid");
         document.getElementById("errorPasswordConf").innerHTML = "Password error";
+        isRight=false;
     }
 
 
     if (provinciaReg.value == ""){
         provinciaReg.classList.add("is-invalid");
         document.getElementById("errorProvincia").innerHTML = "Camp obligatori";
+        isRight=false;
     }
+
+    return isRight;
 }
